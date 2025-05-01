56dce5ea7b956b56796597b3802140f9664cf06d
MuhShahram <mshahram574@gmail.com>
2025-05-01 16:09:09 +0500

    fix home page images

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
         "Homemade-Apple": ["Homemade Apple", "cursive"],
       },
       backgroundImage: {
-        "hero-image": "url('../public/imgs/Hero  Section (1).png')",
+        "hero-image": "url('./imgs/main-hero-image.png')",
       },
       maxWidth: {
         custom: "1200px",
