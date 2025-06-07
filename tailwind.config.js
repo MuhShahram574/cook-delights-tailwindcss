3c78b6d17dfb1e980bc67e90ba2b930b87d8b3ed
MuhShahram <mshahram574@gmail.com>
2025-06-07 19:40:12 +0500

    add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,7 +44,44 @@ module.exports = {
         "sm-height-card": "320px",
         "lg-height-card": "250px",
       },
+      animation: {
+        appear: "appear 4s forwards",
+        fadeIn: "fadeIn linear 2s",
+        fadeOut: "fadeOut linear 2s",
+        flowerOpen: "flowerOpen 2s forwards",
+        "slide-left": "slide-left 2s forwards", // Adjust duration as needed
+        "slide-right": "slide-right 2s forwards", // Adjust duration as needed
+        "slide-left-fast": "slide-left-fast forwards",
+        "slide-bottom": "slide-bottom 2s forwards",
+        "circle-open": "circle-Open 2s forwards",
+        "slide-top": "slide-top 2s forwards",
+        "spin-slow": "spin 7s linear infinite", // Adjust the duration (5 seconds)
+        // "animation-timeline": "view()",
+        // "animation-range": " entry 0% cover 40%",
+      },
+      fadeOut: {
+        "0%": {
+          opacity: "0",
+          transform: "translateX(200px)",
+        },
+        "100%": {
+          opacity: "1",
+          transform: "translateX(0px)",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".scroll-animate": {
+          animationTimeline: "view()",
+          animationRange: "entry 0% cover 40%",
+        },
+      };
+
+      // Add utilities with responsive and hover variants
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
